a4f838a0185445d9d6d9d1a17ab01fe636620ef1
Arlind Gurgurovci <arlind.gurgurovci@gmail.com>
2026-04-22 17:18:27 +0200

    added json

--- lib/api/user/log.data.ts
+++ lib/api/user/log.data.ts
@@ -1,16 +1,287 @@
-export interface ItemMarketSellOldData {
+const logTypes = [
+        {
+            "id": 1104,
+            "title": "Item market sell (old)"
+    },
+        {
+            "id": 1113,
+            "title": "Item market sell"
+    },
+        {
+            "id": 1221,
+            "title": "Bazaar sell (legacy)"
+    },
+        {
+            "id": 1226,
+            "title": "Bazaar sell"
+    },
+        {
+            "id": 2405,
+            "title": "Item use wallet"
+    },
+        {
+            "id": 2407,
+            "title": "Item use stash box"
+    },
+        {
+            "id": 4210,
+            "title": "Item shop sell"
+    },
+        {
+            "id": 4220,
+            "title": "Item shop sell points"
+    },
+        {
+            "id": 4441,
+            "title": "Trade money incoming"
+    },
+        {
+            "id": 4510,
+            "title": "Ammo sell"
+    },
+        {
+            "id": 4810,
+            "title": "Money receive"
+    },
+        {
+            "id": 5011,
+            "title": "Points market sell"
+    },
+        {
+            "id": 5451,
+            "title": "Bank withdraw"
+    },
+        {
+            "id": 5460,
+            "title": "Cashiers check withdraw"
+    },
+        {
+            "id": 5511,
+            "title": "Stock sell"
+    },
+        {
+            "id": 5531,
+            "title": "Stock special money"
+    },
+        {
+            "id": 5720,
+            "title": "Crime success money gain"
+    },
+        {
+            "id": 5928,
+            "title": "Property sell"
+    },
+        {
+            "id": 5937,
+            "title": "Property rental market rent owner"
+    },
+        {
+            "id": 5943,
+            "title": "Property rental market extension accept owner"
+    },
+        {
+            "id": 6012,
+            "title": "Offshore bank interest"
+    },
+        {
+            "id": 6020,
+            "title": "Hunting"
+    },
+        {
+            "id": 6200,
+            "title": "Loan increase"
+    },
+        {
+            "id": 6220,
+            "title": "Job pay"
+    },
+        {
+            "id": 6221,
+            "title": "Company employee pay"
+    },
+        {
+            "id": 6285,
+            "title": "Company withdraw"
+    },
+        {
+            "id": 6300,
+            "title": "Company sell"
+    },
+        {
+            "id": 6404,
+            "title": "Job special gain money"
+    },
+        {
+            "id": 6407,
+            "title": "Job special class action lawsuit"
+    },
+        {
+            "id": 6509,
+            "title": "Company special gain money"
+    },
+        {
+            "id": 6539,
+            "title": "Company special use intricate hack"
+    },
+        {
+            "id": 6549,
+            "title": "Company special use logistics report"
+    },
+        {
+            "id": 6708,
+            "title": "Bounty refund"
+    },
+        {
+            "id": 6710,
+            "title": "Bounty claim"
+    },
+        {
+            "id": 6736,
+            "title": "Faction give money receive"
+    },
+        {
+            "id": 6754,
+            "title": "Faction money receive ownership"
+    },
+        {
+            "id": 6793,
+            "title": "Faction payout money receive"
+    },
+        {
+            "id": 6811,
+            "title": "Faction payday receive"
+    },
+        {
+            "id": 7125,
+            "title": "Image advert refund"
+    },
+        {
+            "id": 7815,
+            "title": "Missions complete"
+    },
+        {
+            "id": 8155,
+            "title": "Attack mug"
+    },
+        {
+            "id": 8165,
+            "title": "Attack arrest"
+    },
+        {
+            "id": 8300,
+            "title": "Casino slots win"
+    },
+        {
+            "id": 8305,
+            "title": "Casino roulette win"
+    },
+        {
+            "id": 8314,
+            "title": "Casino high-low cash in full"
+    },
+        {
+            "id": 8315,
+            "title": "Casino high-low cash in half"
+    },
+        {
+            "id": 8320,
+            "title": "Casino keno win"
+    },
+        {
+            "id": 8331,
+            "title": "Casino craps win"
+    },
+        {
+            "id": 8355,
+            "title": "Casino blackjack win"
+    },
+        {
+            "id": 8357,
+            "title": "Casino blackjack insurance win"
+    },
+        {
+            "id": 8358,
+            "title": "Casino blackjack push"
+    },
+        {
+            "id": 8359,
+            "title": "Casino blackjack surrender"
+    },
+        {
+            "id": 8374,
+            "title": "Casino spin the wheel win money"
+    },
+        {
+            "id": 8395,
+            "title": "Casino russian roulette win"
+    },
+        {
+            "id": 8399,
+            "title": "Casino russian roulette timeout refund"
+    },
+        {
+            "id": 8400,
+            "title": "Casino russian roulette leave"
+    },
+        {
+            "id": 8411,
+            "title": "Casino poker table leave"
+    },
+        {
+            "id": 8413,
+            "title": "Casino poker tournament unregister"
+    },
+        {
+            "id": 8444,
+            "title": "Casino poker tournament winnings"
+    },
+        {
+            "id": 8455,
+            "title": "Bookie withdraw"
+    },
+        {
+            "id": 8465,
+            "title": "Bookie withdraw (new)"
+    },
+        {
+            "id": 8741,
+            "title": "Racing receive refund"
+    },
+        {
+            "id": 8940,
+            "title": "Christmas town money"
+    },
+        {
+            "id": 8960,
+            "title": "Easter egg hunt pickup blue egg (legacy)"
+    },
+        {
+            "id": 9015,
+            "title": "Crime success money gain (new)"
+    },
+        {
+            "id": 9052,
+            "title": "Crime success bootlegging sell DVDs"
+    },
+        {
+            "id": 9056,
+            "title": "Crime success skimming sell card details"
+    }
+    ] as const
+
+export interface ItemMarketSellOldData { //1104
 
 }
 
-export interface ItemMarketSellData {
+export interface ItemMarketSellData { //1113
 
 }
 
-export interface BazaarSellLegacyData {
+export interface BazaarSellLegacyData { //1221
 
 }
 
-export interface BazaarSellData {
+export interface BazaarSellData { //1226
 
 }
 
